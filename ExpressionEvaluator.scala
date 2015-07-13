bc30d40682218c734679425caf5ad86181487b6f
Andrew Hancock <AndrewNeilHancock@gmail.com>
2015-07-12 22:07:30 -0400

    1. Modified grammar to support more complex expressions. 2. Added Sub and Div operations.

--- ExpressionEvaluator.scala
+++ ExpressionEvaluator.scala
@@ -1,12 +1,12 @@
 /** Grammar:
-  *    Expression :: = Term | Term + Factor | Term - Factor
+  *    Expression :: = Expression + Term | Expression - Term
   *    Term ::= Factor | Term * Factor | Term / Factor
   *	   Factor :: = Constant
   *	   Constant :: = Int
   **/	   
 abstract class Expression
-case class Add(term: Term, expression: Expression) extends Expression
-case class Sub(term: Term, expression: Expression) extends Expression
+case class Add(expression: Expression, term: Term) extends Expression
+case class Sub(expression: Expression, term: Term) extends Expression
 
 abstract class Term extends Expression
 case class Mul(term: Term, factor: Factor) extends Term
@@ -21,11 +21,17 @@ object ExpressionEvaluator {
 		// Define AST for: 2 + 3 * 5		
 		val expr = Add(Constant(2), Mul(Constant(3), Constant(5)))
 		println(eval(expr))
+		
+		// define AST for: 2 + 3 * 5 - 4 / 2
+		val expr2 = Sub(Add(Constant(2), Mul(Constant(3), Constant(5))), Div(Constant(4), Constant(2)))
+		println(eval(expr2))
 	}
 	
 	def eval(expression: Expression): Int  = expression match {
 		case Add(x, y) => eval(x) + eval(y)		 
+		case Sub(x, y) => eval(x) - eval(y)
 		case Mul(x, y) => eval(x) * eval(y)
+		case Div(x, y) => eval(x) / eval(y)
 		case Constant(value) => value			
 	}
 }
